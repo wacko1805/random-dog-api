eebb30668abc6d364cd41ad4cb15b2c2de06d9df
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-07-04 20:18:20 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("content-type: application/json");
  echo "[" ;
  echo file_get_contents( "https://dog.ceo/api/breeds/image/random" ); 
  echo "]";
   ?>
+
+
 
